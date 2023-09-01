77d7157a9cf1c06f4567129596b4fb8b79f8d90e
Manasa-dev503 <198a1a0503manasa@gmail.com>
2023-09-01 17:32:26 +0530

    fdfe

--- App.js
+++ App.js
@@ -1,22 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Counter from './Counter';
-import Todolist from './Todolist';
-import Textbox from './Textboxarray';
-import Texthead from './Textboxheading';
-import Textlist from './Textboxlist';
-import Tododelete from './Todolistdelete';
+
+import
 
 function App() {
   return (
     <div className="mybox">
-      <h1>ReactJS</h1>
-      <Counter></Counter>
-      <Todolist></Todolist>
-      <Textbox></Textbox>
-      <Texthead></Texthead>
-      <Textlist></Textlist>
-      <Tododelete></Tododelete>
+      <Review></Review>
     </div>
   );
 }
